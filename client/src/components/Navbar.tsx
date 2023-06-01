3eb75273acadaa1397700304cc8d3aa53885ccbc
Abrar Hussain <117599985+sahussain887@users.noreply.github.com>
2023-06-01 13:12:25 -0500

    navbar and links added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
-    <div>
-      <h1 className="navbar">Navbar</h1>
-      <h1>abrar</h1>
-    </div>
-  )
-}
+    <nav style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', background: 'white', padding: '1rem', borderBottom: '3px solid rgb(224,224,224)' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <ul style={{ display: 'flex', alignItems: 'center', gap: '1rem', marginLeft: '2rem' }}>
+          <li>
+            <a href="/" style={{ color: 'black', fontWeight: 'bold' }}>
+              <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/2034px-Starbucks_Corporation_Logo_2011.svg.png" alt="Starbucks Logo" style={{ height: '40px' }} />
+            </a>
+          </li>
+          <li>
+            <a href="/menu" style={{ color: 'black', fontWeight: 'bold', marginLeft: '2rem' }}>MENU</a>
+          </li>
+          <li>
+            <a href="/rewards" style={{ color: 'black', fontWeight: 'bold', marginLeft: '2rem' }}>REWARDS</a>
+          </li>
+          <li>
+            <a href="/gifts" style={{ color: 'black', fontWeight: 'bold', marginLeft: '2rem' }}>GIFT CARDS</a>
+          </li>
+        </ul>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center', marginRight: '2rem' }}>
+          <a href="/store-locator" style={{ color: 'black' }}>Find Store</a>
+        </div>
+        <ul style={{ display: 'flex', gap: '1rem' }}>
+          <li>
+            <a href="/signin" style={{ color: 'black', display: 'flex', alignItems: 'center' }}>
+              <button style={{ color: 'black', padding: '0.5rem', borderRadius: '10rem', border: '1px solid black', marginRight: '2rem' }}>Sign In</button>
+            </a>
+          </li>
+          <li>
+            <a href="/account/create" style={{ color: 'black', display: 'flex', alignItems: 'center' }}>
+              <button style={{ background: 'black', color: 'white', padding: '0.5rem', borderRadius: '10rem', border: 'none', marginRight: '2rem' }}>Join Now</button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/views/Gifts.tsx
+++ client/src/views/Gifts.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Gifts = () => {
   return (
     <div>
-        <h1>Home</h1>
+        <h1>Gifts</h1>
     </div>
   )
 }
 
-export default Home
+export default Gifts
