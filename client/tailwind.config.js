2ff99667a5e5d4d37ca121bf8151d40e1686848e
Abrar Hussain <117599985+sahussain887@users.noreply.github.com>
2023-06-02 01:04:03 -0500

    Home page completed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'sodo-sans': ['SoDoSans', 'sans-serif'],
+    },
   },
   plugins: [],
 }
