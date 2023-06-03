a085c7e807daafc75d181791701c1de2a8d2c87f
JordanNitta <Jordannittarodrigues@gmail.com>
2023-06-02 22:19:56 -0700

    Still a work in progress

--- client/src/components/Rewards/CashorCard.tsx
+++ client/src/components/Rewards/CashorCard.tsx
@@ -8,12 +8,20 @@ const CashorCard = () => {
                 <p className='font-main'>No matter how you pay, you can earn Stars with your morning coffee.</p>
                 <p className='font-main'>Those Stars add up to (really delicious) Rewards.</p>
                 <div className='flex flex-col md:flex-row md:px-12'>
-                    <div className='flex-col text-start -space-y-10 pl-3 md:-space-y-3 md:w-1/2'>
+                    <div className='flex-col text-start -space-y-10 pl-3 md:-space-y-3 '>
                         <h2 className='font-soDo font-semibold text-[19px] py-10 md:text-[28px] md:py-5'>1 per dollar</h2>
                         <p>Pay as you go</p>
                     </div>
-                    <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1A.png" alt="scan and pay" width={112} />
-                    <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1B.png" alt="scan and pay" width={112} />
+                    <div className='flex p-[24px]'>
+                        <div className='flex'>
+                            <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1A.png" alt="scan and pay" width={112} />
+                            <div className='text-start'>
+                                <h2>Scan and pay separately</h2>
+                                <p>Use cash or credit/debit card at the register.</p>
+                            </div>
+                        </div>
+                        <img src="https://www.starbucks.com/weblx/images/rewards/ways-to-pay/1B.png" alt="scan and pay" width={112} />
+                    </div>
                 </div>
             </div>
         </div>
