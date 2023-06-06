b1935e20ab1466ed14646841e01c8b70b5059c6c
Abrar Hussain <117599985+sahussain887@users.noreply.github.com>
2023-06-05 19:28:28 -0500

    menu page completed

--- client/src/views/MenuPrev.tsx
+++ client/src/views/MenuPrev.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MenuNav from "../components/MenuNav";
+import MenuNav from "../components/Menu/MenuNav";
 import { Link } from "react-router-dom";
 const MenuPrev = () => {
   return (
